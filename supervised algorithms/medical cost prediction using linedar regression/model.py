02914d668fff437b9fed3e7fa906dd9f0575f78c
shubhendu ghosh <77840111+shubhendu-ghosh-DS@users.noreply.github.com>
2022-01-15 17:01:59 +0530

    update model.py
    adding comment

--- supervised algorithms/medical cost prediction using linedar regression/model.py
+++ supervised algorithms/medical cost prediction using linedar regression/model.py
@@ -1,5 +1,5 @@
-from sklearn.preprocessing import LabelEncoder
-label=LabelEncoder()
+from sklearn.preprocessing import LabelEncoder              # we want to encode feature values
+label=LabelEncoder()                                        #sex, smoker, region these three column are of object type
 label.fit(data.sex.drop_duplicates())
 data.sex=label.transform(data.sex)
 label.fit(data.smoker.drop_duplicates())
@@ -9,13 +9,13 @@ data.region=label.transform(data.region)
 
 
 
-x=data.drop(['charges'],axis=1)
+x=data.drop(['charges'],axis=1)                                         # preparing training set 
 y=data['charges']
-x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=0)
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=0)                #spliting the datasets into two different sets for training and testing
 
 
 
-
+#we're considering five regression models that are already imported
 
 linreg = LinearRegression()
 rfc = RandomForestRegressor(n_estimators = 500 , max_features = 6 , max_depth = 10 , min_samples_leaf = 6)
@@ -28,7 +28,7 @@ xgb = XGBRegressor(gamma = 0.5)
 
 models = [linreg, rfc, abr, gbr, xgb]
 
-
+#creating a function to find the optimal model 
 def training(model):
     score_dict =[]
     error_dict =[]
@@ -46,10 +46,10 @@ def training(model):
     
     
    
-  
+# Calling the function we just built 
 training(models)
 
-
+#traing the best fit model
 
 RFC = RandomForestRegressor(n_estimators = 500 , max_features = 6 , max_depth = 10 , min_samples_leaf = 6)
 RFC.fit(x_train , y_train)
